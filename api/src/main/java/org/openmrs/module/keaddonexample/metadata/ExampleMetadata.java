2160d499b46c5fd2aba1fcf73ae58e958d1fcd0e
werick <jackericmugoma@gmail.com>
2013-11-27 13:23:40 +0300

    updates

--- api/src/main/java/org/openmrs/module/keaddonexample/metadata/ExampleMetadata.java
+++ api/src/main/java/org/openmrs/module/keaddonexample/metadata/ExampleMetadata.java
@@ -27,10 +27,12 @@ public class ExampleMetadata extends AbstractMetadataBundle {
 
 	public static class _EncounterType {
 		public static final String EXAMPLE = "d69dedbd-3933-4e44-8292-bea939ce980a";
+        public static final String ADHERENCE = "155c23a5-a6e3-4d33-aceb-7a1a735c51de";
 	}
 
 	public static class _Form {
 		public static final String EXAMPLE = "b694b1bc-2086-47dd-a4ad-ba48f9471e4b";
+        public static final String ADHERENCE = "db64f32b-1ce5-4767-a0d1-45eeb6b81eb8";
 	}
 
 	/**
@@ -39,7 +41,9 @@ public class ExampleMetadata extends AbstractMetadataBundle {
 	@Override
 	public void install() {
 		install(encounterType("Example encounter", "Just an example", _EncounterType.EXAMPLE));
+        install(encounterType("Adherence encounter", "Adherence visit details", _EncounterType.ADHERENCE));
 
 		install(form("Example form", null, _EncounterType.EXAMPLE, "1", _Form.EXAMPLE));
+        install(form("Adherence form", null, _EncounterType.ADHERENCE, "1", _Form.ADHERENCE));
 	}
 }
