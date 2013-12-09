bd770a4aba76ec02fe5597419ec0a149c89b62c6
werick <jackericmugoma@gmail.com>
2013-12-09 14:39:22 +0300

    Added Moh 257 Arv Therapy for testing with kenyaemr

--- api/src/main/java/org/openmrs/module/keaddonexample/metadata/ExampleMetadata.java
+++ api/src/main/java/org/openmrs/module/keaddonexample/metadata/ExampleMetadata.java
@@ -28,13 +28,17 @@ import static org.openmrs.module.metadatadeploy.bundle.CoreConstructors.*;
 public class ExampleMetadata extends AbstractMetadataBundle {
 
 	public static class _EncounterType {
-		public static final String EXAMPLE = "d69dedbd-3933-4e44-8292-bea939ce980a";
+
         public static final String ADHERENCE = "155c23a5-a6e3-4d33-aceb-7a1a735c51de";
+        public static final String ARV_THERAPY_ENCOUNTER = "aeaaa866-7ed1-455c-9002-ba40e67a9882";
+        public static final String EXAMPLE = "d69dedbd-3933-4e44-8292-bea939ce980a";
 	}
 
 	public static class _Form {
-		public static final String EXAMPLE = "b694b1bc-2086-47dd-a4ad-ba48f9471e4b";
+
         public static final String ADHERENCE = "db64f32b-1ce5-4767-a0d1-45eeb6b81eb8";
+        public static final String ARV_THERAPY_FORM = "d25da84f-46ff-4349-ac7b-09d6e0622c98";
+        public static final String EXAMPLE = "b694b1bc-2086-47dd-a4ad-ba48f9471e4b";
 	}
 
 	/**
@@ -42,10 +46,16 @@ public class ExampleMetadata extends AbstractMetadataBundle {
 	 */
 	@Override
 	public void install() {
-		install(encounterType("Example encounter", "Just an example", _EncounterType.EXAMPLE));
-        install(encounterType("Adherence encounter", "Adherence visit details", _EncounterType.ADHERENCE));
 
-		install(form("Example form", null, _EncounterType.EXAMPLE, "1", _Form.EXAMPLE));
+        //install encounter_types
+        install(encounterType("Adherence encounter", "Adherence visit details", _EncounterType.ADHERENCE));
+        install(encounterType("Arv Therapy Encounter", "Arv Therapy details", _EncounterType.ARV_THERAPY_ENCOUNTER));
+        install(encounterType("Example encounter", "Just an example", _EncounterType.EXAMPLE));
+
+        //Install forms
+
         install(form("Adherence form", null, _EncounterType.ADHERENCE, "1", _Form.ADHERENCE));
+        install(form("Moh257 Arv Therapy form", null, _EncounterType.ARV_THERAPY_ENCOUNTER, "1", _Form.ARV_THERAPY_FORM));
+        install(form("Example form", null, _EncounterType.EXAMPLE, "1", _Form.EXAMPLE));
 	}
 }
